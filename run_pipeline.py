9ebc6b247726ee39fc23b9eff10a4577daeea63a
Adel Nour <ans9868@nyu.edu>
2025-07-09 12:50:47 -0400

    Testing Github actions X1 + Changing print messages

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,5 +1,6 @@
-import subprocess
 import os
+import subprocess
+
 
 def main():
     print("🧠 [RUN_PIPELINE] Starting PySpark EEG pipeline container...")
@@ -8,17 +9,17 @@ def main():
     script_path = os.path.join("src", "digit_flatmap.py")
 
     if not os.path.exists(script_path):
-        print(f"❌ Could not find {script_path}")
+        print(f"[RUN_PIPELINE] Could not find {script_path}")
         return
 
     # Run the script using spark-submit
     try:
         result = subprocess.run(["spark-submit", script_path], check=True)
-        print("✅ PySpark job completed successfully.")
+        print("[RUN_PIPELINE] PySpark job completed successfully!")
     except subprocess.CalledProcessError as e:
-        print("❌ PySpark job failed.")
+        print("[RUN_PIPELINE] PySpark job failed.")
         print(e)
+
 
 if __name__ == "__main__":
     main()
-
